f7a37d6f23fb865676605287ed1a33044d839d41
Ohazulike Stanley <sohazulike@gmail.com>
2025-11-11 05:05:50 -0800

    updated utils

--- utils/api.ts
+++ utils/api.ts
@@ -1,11 +1,12 @@
+
 // utils/api.ts
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000/api",
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL
+    ? `${process.env.NEXT_PUBLIC_API_BASE_URL}/api`
+    : "http://127.0.0.1:8000/api",
   withCredentials: false, // keep this false if you're using JWT (not cookies)
 });
 
 export default API;
-
-
